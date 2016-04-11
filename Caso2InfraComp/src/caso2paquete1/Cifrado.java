e02aab47273cda61807c28ee898cbec889c8823e
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2016-04-11 16:33:18 -0500

    Added files via upload
    Terminado

--- Caso2InfraComp/src/caso2paquete1/Cifrado.java
+++ Caso2InfraComp/src/caso2paquete1/Cifrado.java
@@ -19,17 +19,7 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
-import javax.security.auth.x500.X500Principal;
 
-import org.bouncycastle.asn1.x509.BasicConstraints;
-import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
-import org.bouncycastle.asn1.x509.GeneralName;
-import org.bouncycastle.asn1.x509.GeneralNames;
-import org.bouncycastle.asn1.x509.KeyPurposeId;
-import org.bouncycastle.asn1.x509.KeyUsage;
-import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 public class Cifrado {
 
@@ -42,21 +32,36 @@ public static byte[] descifrar(byte [] data, Key llave, String algoritmo) throws
 	return cipher.doFinal(data); 
 }
 
-public static byte[] cifrar(byte[] data, Key llave, String algoritmo) throws Exception
+public static byte[] cifrar(byte[] data, Key llave, String algoritmo) throws Exception,NoSuchAlgorithmException
 {
 	Cipher cipher = Cipher.getInstance(algoritmo);
 	cipher.init(Cipher.ENCRYPT_MODE, llave);
 	return cipher.doFinal(data);
 }
 
+public static byte[] calcularMac(byte[] mensaje, Key llave, String algortmo) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, UnsupportedEncodingException {
+    Mac mac = Mac.getInstance(algortmo);
+    mac.init(llave);
+    return mac.doFinal(mensaje);
+     
+}
 
-
-public static byte[] cifradoSimetrico(byte msg[], Key key, String algo)
+public static byte[] cifradoSimetrico(byte mensaje[], Key llave, String algoritmo)
         throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException
     {
-        algo = (new StringBuilder(String.valueOf(algo))).append(!algo.equals("DES") && !algo.equals("AES") ? "" : "/ECB/PKCS5Padding").toString();
-        Cipher decifrador = Cipher.getInstance(algo);
-        decifrador.init(Cipher.ENCRYPT_MODE, key);
-        return decifrador.doFinal(msg);
+        
+		if (algoritmo.equals("DES"))
+		{
+			algoritmo+="/ECB/PKCS5Padding";
+		}
+		else if (algoritmo.equals("AES") )
+		{
+			algoritmo+="/ECB/PKCS5Padding";
+		}
+	
+		
+        Cipher decifrador = Cipher.getInstance(algoritmo);
+        decifrador.init(Cipher.ENCRYPT_MODE, llave);
+        return decifrador.doFinal(mensaje);
     }
 }
